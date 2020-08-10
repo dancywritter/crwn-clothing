cf18dc148e9df833bda521061810572eddf83897
Rodrigo Gimenez <rodrigog83@gmail.com>
2020-08-10 11:45:20 +0200

    Add collection preview and collection item and img

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+import { shopData } from './shop.data';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
+const ShopPage = (props) => {
+  const [collections] = useState(shopData);
+  return (
+    <div className="shop-page">
+      {collections.map(({ id, ...collection }) => (
+        <CollectionPreview key={id} {...collection} />
+      ))}
+    </div>
+  );
+};
+
+export default ShopPage;
