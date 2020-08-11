35291752bec0f1125677dcd234fd8fe6b9e5338b
Rodrigo Gimenez <rodrigog83@gmail.com>
2020-08-11 20:19:59 +0200

    Create stripe payment

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -8,6 +8,9 @@ import {
   selectCartTotal,
 } from '../../redux/cart/cart.selectors';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
 const CheckoutPage = () => {
   const headerBlocks = [
@@ -20,7 +23,9 @@ const CheckoutPage = () => {
 
   const cartItems = useSelector(selectCartItems);
   const total = useSelector(selectCartTotal);
-
+  const stripePromise = loadStripe(
+    'pk_test_51HF0VmGqoQrIWsXCZQQILWFFYUNwsyJ0F15tGg0wd19GfG5Cily39DJaZmQsxS6S7XBjyt12sJFOt7545uOKrcRR00Co2BZZSO'
+  );
   return (
     <div className="checkout-page">
       <div className="checkout-header">
@@ -38,6 +43,14 @@ const CheckoutPage = () => {
       <div className="total">
         <span>TOTAL: ${total}</span>
       </div>
+      <div className="test-warning">
+        *Please use the following test credit card for payments
+        <br />
+        4242 4242 4242 4242 - Exp: 01/21 - CVV: 123
+      </div>
+      <Elements stripe={stripePromise}>
+        <StripeCheckoutButton price={total} />
+      </Elements>
     </div>
   );
 };
